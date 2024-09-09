7bbd84367162e44b4a9a1402a282d6bb811f5711
Osayd <osaidhamidi@gmail.com>
2024-09-09 05:11:00 +0300

    model & setting

--- signlog/settings.py
+++ signlog/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'myapp.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
